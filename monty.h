710561ca94c7580d3931dccc6e321aa723009509
Unarine <UnarineMunyai28@gmail.com>
2023-12-17 11:40:04 -0800

    pp

--- monty.h
+++ monty.h
@@ -31,7 +31,7 @@ typedef struct instruction_s
  void push(stack_t **stack, unsigned int line_number);
  void pall(stack_t **stack, unsigned int line_number);
  void pint(stack_t **stack, unsigned int line_number);
- void pop_top(stack_t **stack, unsigned int line_number);
+ void pop(stack_t **stack, unsigned int line_number);
  void swap(stack_t **stack, unsigned int line_number);
  void add(stack_t **stack, unsigned int line_number);
  void nop(stack_t **stack, unsigned int line_number);
@@ -45,4 +45,4 @@ typedef struct instruction_s
  void rotr(stack_t **stack, unsigned int line_number);
  void stack(stack_t **stack, unsigned int line_number);
  void queue(stack_t **stack, unsigned int line_number);
-#endif
+#endiif
